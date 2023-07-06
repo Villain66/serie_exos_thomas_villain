0e757663bfacdb7a882e7ba91c37d3f4ec9cd3c3
Villain66 <124810879+Villain66@users.noreply.github.com>
2023-07-06 13:32:13 +0200

    fin de l'exercice 4

---
+++ exercice4/exercice4.js
@@ -0,0 +1,3 @@
+let base = parseInt(prompt("Entrez la base du triangle en cm"));
+let hauteur = parseInt(prompt("Entrez la hauteur du triangle en cm"));
+console.log("L' aire du triangle est : " + base * hauteur / 2);
