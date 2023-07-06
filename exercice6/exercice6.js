7dae2744117ef3e1ee281517ac74508ec6e06e35
Villain66 <124810879+Villain66@users.noreply.github.com>
2023-07-06 13:46:22 +0200

    fin de l'exercice 6

---
+++ exercice6/exercice6.js
@@ -0,0 +1,6 @@
+let chiffre = parseInt(prompt("Entrez votre chiffre"));
+if (chiffre % 2 === 0) {
+    console.log("Le chiffre " + chiffre + " est pair.");
+} else {
+  console.log("Le chiffre " + chiffre + " est impair.");
+}
